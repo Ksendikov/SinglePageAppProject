b51d9bde31427709016c811d610913a40bc0d973
Igor <iksendikov@gmail.com>
2021-06-21 14:32:51 +0300

    Added pagination of pages

--- SpaProject/mainspa/api/views.py
+++ SpaProject/mainspa/api/views.py
@@ -5,11 +5,21 @@ from ..models import Table
 from .filter import TableFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+
+
+class TablePagination(PageNumberPagination):
+
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
 
 class TableViewSet(viewsets.ModelViewSet):
 
     queryset = Table.objects.all()
     serializer_class = TableSerializer
+    pagination_class = TablePagination
 
     filter_backends = (DjangoFilterBackend, )
     filterset_class = TableFilter
