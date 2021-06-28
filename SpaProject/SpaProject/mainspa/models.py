882303a1f3bd91b0d0b4160372efac786a294ee8
Igor <iksendikov@gmail.com>
2021-06-28 17:13:01 +0300

    added limit integerfield in models.py

--- SpaProject/SpaProject/mainspa/models.py
+++ SpaProject/SpaProject/mainspa/models.py
@@ -1,17 +1,19 @@
-from django.db import models
 import datetime
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Table(models.Model):
 
     date = models.DateField('Date', default=datetime.date.today())
     name = models.CharField(max_length=150)
-    quantity = models.PositiveIntegerField('quantity', max_length = 1000)
-    distance = models.PositiveIntegerField('distance', max_length=15000)
+    quantity = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(1500)])
+    distance = models.PositiveIntegerField(validators = [MinValueValidator(0), MaxValueValidator(15000)])
 
     class Meta:
-        verbose_name = 'Table'
-        verbose_name_plural = 'Tables'
+        verbose_name = 'Field table'
+        verbose_name_plural = 'Fields table'
+        ordering = ('-date',)
 
     def __str__(self):
         return self.name
